bc42ca556a9a3962b2b3faa0671974d0b83f0c1e
Eduardo <ing.valen0421@hotmail.com>
2021-04-21 17:05:23 -0400

    Se realizaron cambios, basicamente se pueden utilizar todo lo que nos ofrece js de por si
    ahora vamos con ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  name: string = "Jhon Carter";
+  age: number = 3;
   users: string[] = ['Shirley', 'Poppy', 'Luna'];
 
   public sayHello() {
@@ -16,10 +18,17 @@ export class AppComponent {
   public deleteUser(user: string) {
     for (let i = 0; i < this.users.length; i++) {
       if (user == this.users[i]) {
-
         //practicando los metodos de array de js | splice: cual y cuantos
         this.users.splice(i, 1);
       }
     }
+  }
+
+  addUser(newUser: any){
+    this.users.push(newUser);
+    newUser.value = "";
+    // newUser.focus();
+    //cancele el evento de resetear la pag
+    return false;
   }
 }
